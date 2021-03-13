a75cdf21da307b8f89862f57681c6ded741a2dc8
Okunola Stephen <okunolas21@gmail.com>
2021-03-13 16:33:11 +0100

    Added display functions

---
+++ script.js
@@ -0,0 +1,39 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//Event listeners
+todoButton.addEventListener("click", addTodo);
+
+
+//functions
+function addTodo(event){
+    //Prevent form from submiting
+    event.preventDefault();
+    //Create the Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+    //Check Trash 
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    //Append to list
+    todoList.appendChild(todoDiv);
+
+    //Clear todoInput value
+    todoInput.value = "";
+}
